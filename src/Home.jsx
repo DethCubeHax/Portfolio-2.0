a52dc61a9d6df464ff72fbf02eb8004b3fa0d799
DethCubeHax <ultimate.nafis.bhadra11@gmail.com>
2023-09-08 23:15:05 +0800

    Added my logo

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,33 @@
+import { createRoot } from 'react-dom/client'
+import React, { useMemo, useRef, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import * as THREE from 'three'
+import './Home.css'
+
+import DethCubeHax from './assets/DethCubeHax.png';
+
+function Asteroid() {
+    const DethCubeLogo = new THREE.TextureLoader().load(DethCubeHax);
+    return(
+        <mesh>
+            <planeGeometry attach="geometry" args={[2, 2]} />
+            <meshBasicMaterial attach="material" map={DethCubeLogo} transparent />
+        </mesh>
+    )
+}
+
 const Home = () => {
+    // Add the asteroids to the canvas
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="Home">
+            <Canvas>
+                <ambientLight />
+                <pointLight position={[10, 10, 10]} />                
+                <Asteroid />
+            </Canvas>
+        
         </div>
     );
-}
+};
 
 export default Home;
