98b67c01d1cbb0a5a456b2f3caa27711fbee7724
DethCubeHax <ultimate.nafis.bhadra11@gmail.com>
2023-11-13 21:36:44 +0800

    Testing new blog style, scrap old ones

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,94 +1,36 @@
 import React, { useMemo, useRef, useState } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber';
-import * as THREE from 'three';
 import './Home.css';
 
 import DethCubeHax from './assets/DethCubeHax.png';
-
-// Easing function: easeOutQuad
-function easeOutQuad(t) {
-  return t * (2 - t);
-}
-
-function Logo({ onClick }) {
-  const DethCubeLogo = useMemo(() => new THREE.TextureLoader().load(DethCubeHax), []);
-  const meshRef = useRef();
-  const initialScale = 1; // Initial scale of the mesh
-  const targetScale = 5; // Target scale for zooming
-  const animationDuration = 1000; // Animation duration in milliseconds
-
-  const handleZoomIn = () => {
-    const initialTime = performance.now();
-    const animate = (time) => {
-      const elapsedTime = time - initialTime;
-      const progress = Math.min(elapsedTime / animationDuration, 1); // Normalize progress from 0 to 1
-      const easedProgress = easeOutQuad(progress); // Apply easing function
-
-      meshRef.current.scale.set(
-        initialScale + (targetScale - initialScale) * easedProgress,
-        initialScale + (targetScale - initialScale) * easedProgress,
-        initialScale + (targetScale - initialScale) * easedProgress
-      );
-
-      if (progress < 1) {
-        requestAnimationFrame(animate);
-      }
-    };
-
-    requestAnimationFrame(animate);
-  };
-
-  const handleZoomOut = () => {
-    const initialTime = performance.now();
-    const animate = (time) => {
-      const elapsedTime = time - initialTime;
-      const progress = Math.min(elapsedTime / animationDuration, 1); // Normalize progress from 0 to 1
-      const easedProgress = easeOutQuad(progress); // Apply easing function
-
-      meshRef.current.scale.set(
-        targetScale - (targetScale - initialScale) * easedProgress,
-        targetScale - (targetScale - initialScale) * easedProgress,
-        targetScale - (targetScale - initialScale) * easedProgress
-      );
-
-      if (progress < 1) {
-        requestAnimationFrame(animate);
-      }
-    };
-
-    requestAnimationFrame(animate);
-  };
-
-  const handleClick = () => {
-    if (meshRef.current.scale.x === initialScale) {
-      handleZoomIn();
-    } else {
-      handleZoomOut();
-    }
-  };
-
-  return (
-    <mesh ref={meshRef} onClick={handleClick}>
-      <planeGeometry attach="geometry" args={[2, 2]} />
-      <meshBasicMaterial attach="material" map={DethCubeLogo} transparent />
-    </mesh>
-  );
-}
+import LinkedIn from './assets/LinkedIn.png';
 
 const Home = () => {
-  const [zoomedIn, setZoomedIn] = useState(false);
-
-  const handleUsernameClick = () => {
-    setZoomedIn(!zoomedIn);
-  };
 
   return (
     <div className="Home">
-      <Canvas>
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-        {zoomedIn ? null : <Logo />}
-      </Canvas>
+      <div className="HomeHeader">
+        <div></div>
+        <div className="HomeHeaderTitle">
+          <div className="HomeHeaderTitleHolder">
+            <div className="HomeHeaderTitleFN">
+              Nafis
+            </div>
+            <div className="HomeHeaderTitleMN">
+              ul
+            </div>
+            <div className="HomeHeaderTitleLN">
+              Islam
+            </div>
+          </div>
+          <div className="HomeHeaderTitleButtonHolder">
+            <a href="https://www.linkedin.com/in/nafis-ul-islam-207932230/">
+              <img src={LinkedIn} className="HomeHeaderTitleButton" />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="CoreGrid">
+      </div>
     </div>
   );
 };
