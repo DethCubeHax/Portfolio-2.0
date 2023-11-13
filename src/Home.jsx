054e7aa6de6e0b6d79c8063a7bc202437900ad86
DethCubeHax <ultimate.nafis.bhadra11@gmail.com>
2023-11-13 16:34:47 +0800

    Added a progress bar for your birthday :)))

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,18 +5,70 @@ import './Home.css';
 
 import DethCubeHax from './assets/DethCubeHax.png';
 
+// Easing function: easeOutQuad
+function easeOutQuad(t) {
+  return t * (2 - t);
+}
+
 function Logo({ onClick }) {
   const DethCubeLogo = useMemo(() => new THREE.TextureLoader().load(DethCubeHax), []);
   const meshRef = useRef();
   const initialScale = 1; // Initial scale of the mesh
   const targetScale = 5; // Target scale for zooming
+  const animationDuration = 1000; // Animation duration in milliseconds
 
   const handleZoomIn = () => {
-    meshRef.current.scale.set(targetScale, targetScale, targetScale); // Set the scale to zoom in
+    const initialTime = performance.now();
+    const animate = (time) => {
+      const elapsedTime = time - initialTime;
+      const progress = Math.min(elapsedTime / animationDuration, 1); // Normalize progress from 0 to 1
+      const easedProgress = easeOutQuad(progress); // Apply easing function
+
+      meshRef.current.scale.set(
+        initialScale + (targetScale - initialScale) * easedProgress,
+        initialScale + (targetScale - initialScale) * easedProgress,
+        initialScale + (targetScale - initialScale) * easedProgress
+      );
+
+      if (progress < 1) {
+        requestAnimationFrame(animate);
+      }
+    };
+
+    requestAnimationFrame(animate);
+  };
+
+  const handleZoomOut = () => {
+    const initialTime = performance.now();
+    const animate = (time) => {
+      const elapsedTime = time - initialTime;
+      const progress = Math.min(elapsedTime / animationDuration, 1); // Normalize progress from 0 to 1
+      const easedProgress = easeOutQuad(progress); // Apply easing function
+
+      meshRef.current.scale.set(
+        targetScale - (targetScale - initialScale) * easedProgress,
+        targetScale - (targetScale - initialScale) * easedProgress,
+        targetScale - (targetScale - initialScale) * easedProgress
+      );
+
+      if (progress < 1) {
+        requestAnimationFrame(animate);
+      }
+    };
+
+    requestAnimationFrame(animate);
+  };
+
+  const handleClick = () => {
+    if (meshRef.current.scale.x === initialScale) {
+      handleZoomIn();
+    } else {
+      handleZoomOut();
+    }
   };
 
   return (
-    <mesh ref={meshRef} onClick={handleZoomIn}>
+    <mesh ref={meshRef} onClick={handleClick}>
       <planeGeometry attach="geometry" args={[2, 2]} />
       <meshBasicMaterial attach="material" map={DethCubeLogo} transparent />
     </mesh>
@@ -27,7 +79,7 @@ const Home = () => {
   const [zoomedIn, setZoomedIn] = useState(false);
 
   const handleUsernameClick = () => {
-    setZoomedIn(true);
+    setZoomedIn(!zoomedIn);
   };
 
   return (
--- src/Love.jsx
+++ src/Love.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Progress } from 'react-sweet-progress';
 import './Love.css';
 
 function Love() {
@@ -7,12 +8,13 @@ function Love() {
   const [timeSince, setTimeSince] = useState({
     months: 0,
     days: 0,
-    hours: 0,
+    hours: 0, 
     seconds: 0,
   });
   const [imageIndex, setImageIndex] = useState(0);
   const [imageFadeIn, setImageFadeIn] = useState(true);
   const [textVisible, setTextVisible] = useState(false);
+  const [additionalDivVisible, setAdditionalDivVisible] = useState(false);
   const [images] = useState([
     'tusi-min-1.png',
     'tusi-min-2.png',
@@ -63,7 +65,7 @@ function Love() {
       setTimeout(() => {
         const nextImageIndex = (imageIndex + 1) % images.length;
         setImageIndex(nextImageIndex);
-        setImageFadeIn(true);
+        setImageFadeIn(true);  
       }, 1000);
     };
 
@@ -77,15 +79,32 @@ function Love() {
   useEffect(() => {
     if (showTime) {
       setTextVisible(true);
+      setTimeout(() => setAdditionalDivVisible(true), 2000);
     }
   }, [showTime]);
+
+  const calculateAge = () => {
+    const birthDate = new Date('January 4, 2004');
+    const currentDate = new Date();
+  
+    const lastBirthday = new Date(currentDate.getFullYear(), 0, 4);
+    
+    const daysSinceLastBirthday = (currentDate - lastBirthday)/(1000 * 60 * 60 * 24);
+  
+    return {
+      years: currentDate.getFullYear() - birthDate.getFullYear(), 
+      daysSinceLastBirthday
+    };
+  };
+
+  const { years, daysSinceLastBirthday } = calculateAge();
 
   return (
     <div>
       <div className="main-body">
         <div className="image-container">
           <img
-            key={imageIndex}
+            key={imageIndex} 
             src={images[imageIndex]}
             alt="Tusi"
             className={`center-image ${imageFadeIn ? 'fade-in' : 'fade-out'}`}
@@ -94,10 +113,27 @@ function Love() {
         <h1>{typedText}</h1>
         {textVisible && (
           <>
-            <h2 className={`fade-in ${textVisible ? 'fade-in' : 'fade-in'}`}>We've been together for</h2>
+            <h2 className={`fade-in ${textVisible ? 'fade-in' : 'fade-in'}`}>We've been together for</h2> 
             <h3 className={`fade-in ${textVisible ? 'fade-in' : 'fade-in'}`}>
               {timeSince.months} months, {timeSince.days} days, {timeSince.hours} hours, and {timeSince.seconds} seconds
             </h3>
+          {additionalDivVisible && (
+
+            <div
+              className={`additional-div fades-in ${
+                additionalDivVisible ? 'fade-in' : '' 
+              }`} 
+            >
+              You are {(daysSinceLastBirthday/365 * 100).toFixed(5)}% to becoming {years} years old!
+
+              <div className="progress-bar-container">
+                <div  
+                  className="progress-bar"
+                  style={{width: `${(daysSinceLastBirthday/365) * 100}%`}}   
+                ></div>
+              </div>
+            </div>
+            )}
           </>
         )}
       </div>
