c23373237d90663ba3ec6d9f9815fbf661039e0e
DethCubeHax <ultimate.nafis.bhadra11@gmail.com>
2023-11-13 23:28:03 +0800

    Added typewriter text

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,16 +1,28 @@
 import React, { useEffect, useRef } from 'react';
 import './Home.css';
 
+
 import DethCubeHax from './assets/DethCubeHax.png';
 import LinkedIn from './assets/LinkedIn.png';
 
 import { initializeParticles } from './components/Particles';
+import typewriter from './components/Typewriter';
 
 const Home = () => {
   const canvasRef = useRef(null);
+  const canvasTextRef = useRef(null);
+
+  const titleFNTextRef = useRef(null);
+  const titleMNTextRef = useRef(null);
+  const titleLNTextRef = useRef(null);
 
   useEffect(() => {
     const cleanup = initializeParticles(canvasRef);
+
+    typewriter(titleFNTextRef.current, "Nafis", 100, 0);
+    typewriter(titleMNTextRef.current, "ul", 100, 0.5);
+    typewriter(titleLNTextRef.current, "Islam", 100, 1);
+    typewriter(canvasTextRef.current, "Hi there! I'm Nafis, aka DethCubeHax, a software engineer in the making.", 100, 2);
 
     return () => {
       cleanup();
@@ -23,15 +35,9 @@ const Home = () => {
         <div></div>
         <div className="HomeHeaderTitle">
           <div className="HomeHeaderTitleHolder">
-            <div className="HomeHeaderTitleFN">
-              Nafis
-            </div>
-            <div className="HomeHeaderTitleMN">
-              ul
-            </div>
-            <div className="HomeHeaderTitleLN">
-              Islam
-            </div>
+            <div className="HomeHeaderTitleFN" ref={titleFNTextRef}></div>
+            <div className="HomeHeaderTitleMN" ref={titleMNTextRef}></div>
+            <div className="HomeHeaderTitleLN" ref={titleLNTextRef}></div>
           </div>
           <div className="HomeHeaderTitleButtonHolder">
             <a href="https://www.linkedin.com/in/nafis-ul-islam-207932230/">
@@ -40,6 +46,7 @@ const Home = () => {
           </div>
         </div>
       </div>
+      <div ref={canvasTextRef} className="TypewriterText"></div>
       <canvas ref={canvasRef} className="ParticleCanvas" />
     </div>
   );
