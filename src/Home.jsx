5eb51ae33f93bc5a1941f4249ea2d77fdc9a45a9
DethCubeHax <ultimate.nafis.bhadra11@gmail.com>
2023-11-28 20:20:20 +0800

    Replace 'I do work' with my age because it looks awkward idk

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,6 +7,7 @@ import Research from './assets/Research.png';
 import Art from './assets/Art.png';
 import PhotoFrame from './components/PhotoFrame';
 import Project from './assets/Project.png';
+import Hourglass from './assets/Hourglass.png';
 import Work from './assets/Work.png';
 
 import { initializeParticles } from './components/Particles';
@@ -38,7 +39,7 @@ const Home = () => {
       50,
       2
     );
-    typewriter(manyMore.current, 'And more.', 100, 3);
+    typewriter(manyMore.current, 'And more', 50, 3);
 
     const timer = setTimeout(() => {
       setShowSkills(true);
@@ -48,6 +49,26 @@ const Home = () => {
       clearTimeout(timer);
       cleanup();
     };
+  }, []);
+  
+  const [age, setAge] = useState(0);
+
+  const getAge = () => {
+    const birthDate = new Date('2002-08-17');
+    const today = new Date();
+    const diffInMilliSeconds = Math.abs(today - birthDate);
+    const age = (diffInMilliSeconds / (1000 * 60 * 60 * 24 * 365.25)).toFixed(2); // convert milliseconds to years
+    return age;
+  };
+
+  useEffect(() => {
+    setAge(getAge()); // Set the initial age
+
+    const intervalId = setInterval(() => {
+      setAge(getAge()); // Update age every second
+    }, 0);
+
+    return () => clearInterval(intervalId); // Clear interval on unmount
   }, []);
 
   useEffect(() => {
@@ -95,11 +116,11 @@ const Home = () => {
       {window.innerWidth > 600 && <NavPanel />}
       <canvas ref={canvasRef} className="ParticleCanvas" />
       <div className={`Skills ${showSkills ? 'SkillsVisible' : ''}`}>
-        <a className="TypewriterTextSmall" href="projects">
+        <a className="TypewriterTextSmall">
           <div>
-            <img src={Project} className="SkillIcon" alt="Project" />
+            <img src={Hourglass} className="SkillIcon" alt="Project" />
           </div>
-          I do projects.
+          {`I am ${getAge()} years old.`}
         </a>
         <a className="TypewriterTextSmall" href="research">
           <div>
@@ -107,11 +128,11 @@ const Home = () => {
           </div>
           I do research.
         </a>
-        <a className="TypewriterTextSmall" href="work">
+        <a className="TypewriterTextSmall" href="projects">
           <div>
-            <img src={Work} className="SkillIcon" alt="Art" />
+            <img src={Project} className="SkillIcon" alt="Art" />
           </div>
-          I do work.
+          I do projects.
         </a>
       </div>
 
