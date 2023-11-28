39a69d14db701801d072e6e8354e691c98a395ab
DethCubeHax <ultimate.nafis.bhadra11@gmail.com>
2023-11-28 23:05:22 +0800

    Revert changes to homepage

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,7 +7,6 @@ import Research from './assets/Research.png';
 import Art from './assets/Art.png';
 import PhotoFrame from './components/PhotoFrame';
 import Project from './assets/Project.png';
-import Hourglass from './assets/Hourglass.png';
 import Work from './assets/Work.png';
 
 import { initializeParticles } from './components/Particles';
@@ -39,7 +38,7 @@ const Home = () => {
       50,
       2
     );
-    typewriter(manyMore.current, 'And more.', 50, 3);
+    typewriter(manyMore.current, 'And more.', 100, 3);
 
     const timer = setTimeout(() => {
       setShowSkills(true);
@@ -51,25 +50,13 @@ const Home = () => {
     };
   }, []);
   
-  const [age, setAge] = useState(0);
-
   const getAge = () => {
     const birthDate = new Date('2002-08-17');
     const today = new Date();
     const diffInMilliSeconds = Math.abs(today - birthDate);
-    const age = (diffInMilliSeconds / (1000 * 60 * 60 * 24 * 365.25)).toFixed(2); // convert milliseconds to years
+    const age = (diffInMilliSeconds / (1000 * 60 * 60 * 24 * 365.25)).toFixed(10); // convert milliseconds to years
     return age;
   };
-
-  useEffect(() => {
-    setAge(getAge()); // Set the initial age
-
-    const intervalId = setInterval(() => {
-      setAge(getAge()); // Update age every second
-    }, 0);
-
-    return () => clearInterval(intervalId); // Clear interval on unmount
-  }, []);
 
   useEffect(() => {
     if (showSkills) {
@@ -116,11 +103,11 @@ const Home = () => {
       {window.innerWidth > 600 && <NavPanel />}
       <canvas ref={canvasRef} className="ParticleCanvas" />
       <div className={`Skills ${showSkills ? 'SkillsVisible' : ''}`}>
-        <a className="TypewriterTextSmall">
+        <a className="TypewriterTextSmall" href="projects">
           <div>
-            <img src={Hourglass} className="SkillIcon" alt="Project" />
+            <img src={Project} className="SkillIcon" alt="Project" />
           </div>
-          {`I am ${getAge()} years old.`}
+          I do projects.
         </a>
         <a className="TypewriterTextSmall" href="research">
           <div>
@@ -128,11 +115,11 @@ const Home = () => {
           </div>
           I do research.
         </a>
-        <a className="TypewriterTextSmall" href="projects">
+        <a className="TypewriterTextSmall" href="work">
           <div>
-            <img src={Project} className="SkillIcon" alt="Art" />
+            <img src={Work} className="SkillIcon" alt="Art" />
           </div>
-          I do projects.
+          I do work.
         </a>
       </div>
 
