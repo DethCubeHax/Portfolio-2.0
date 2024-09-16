9784d4d5f5da56233bcc0d311acb366875ed323c
DethCubeHax <ultimate.nafis.bhadra11@gmail.com>
2024-09-16 22:20:36 +0800

    Raksh's birthday

--- src/PageRoutes.jsx
+++ src/PageRoutes.jsx
@@ -5,6 +5,7 @@ import Projects from "./Projects";
 import Work from "./Work";
 import Research from "./Research";
 import Blog from "./Blog";
+import Raksh from "./Raksh";
 
 function PageRoutes() {
     const location = useLocation();
@@ -15,6 +16,7 @@ function PageRoutes() {
             <Route path="/work" element={<Work />} />
             <Route path="/blog" element={<Blog />} />
             <Route path="/research" element={<Research />} />
+            <Route path="/raksh" element={<Raksh />} />
         </Routes>
     );
 }
