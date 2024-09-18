c2016f0c5b89b290042027c355cd868d1ba4d8b1
DethCubeHax <ultimate.nafis.bhadra11@gmail.com>
2024-09-18 13:37:27 +0800

    Updated resume

--- src/PageRoutes.jsx
+++ src/PageRoutes.jsx
@@ -5,7 +5,6 @@ import Projects from "./Projects";
 import Work from "./Work";
 import Research from "./Research";
 import Blog from "./Blog";
-import Raksh from "./Raksh";
 
 function PageRoutes() {
     const location = useLocation();
@@ -16,7 +15,6 @@ function PageRoutes() {
             <Route path="/work" element={<Work />} />
             <Route path="/blog" element={<Blog />} />
             <Route path="/research" element={<Research />} />
-            <Route path="/raksh" element={<Raksh />} />
         </Routes>
     );
 }
